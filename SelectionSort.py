5bd3ceaa4e8cf6e5721dfc2d263bf9f2375de0e6
bathini79 <116077457+bathini79@users.noreply.github.com>
2023-07-09 00:20:55 +0530

    Update SelectionSort.py

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,1 +1,15 @@
-
+def getMin(arr,i):
+    minimum=arr[i]
+    minIndex=i
+    for j in range(i,len(arr)):
+        if(arr[j]<minimum):
+            minIndex=j
+            minimum=arr[j]
+    return minIndex
+def selectionSort(arr,n):
+    # Write your code here
+    # Do not return anything. Update the given array in-place
+    for i in range(n-1):
+        minimum=getMin(arr,i)
+        arr[i],arr[minimum]=arr[minimum],arr[i]
+    return arr
